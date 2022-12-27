14fbd0e49ff7e7bfa4b35d0d19e8e954309e31af
Naviralobo <majel.josephine>
2022-12-27 15:13:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import Datetime from "react-datetime";
 
 function App() {
+  const [startDate, setStartDate] = useState(null);
+  const [duration, setDuration] = useState(0);
+  const [resultDate, setResultDate] = useState(startDate);
+  const dateChangeHandler = (value) => {
+    setStartDate(value);
+  };
+  const resultDateHandler = (e) => {
+    setDuration(e.target.value);
+    console.log(startDate);
+    console.log(duration);
+    let newDate = new Date(startDate);
+    let d = newDate.setDate(newDate.getDate() + duration);
+    setResultDate(new Date(d));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <div>
+        <h6>From Date</h6>
+        <DatePicker
+          selected={startDate}
+          onChange={dateChangeHandler}
+          dateFormat="MM/dd/yyyy"
+        />
+      </div>
+      <div>
+        <h6>Duration(days)</h6>
+        <input type="number" value={duration} onChange={resultDateHandler} />
+      </div>
+      <div>
+        <h6>To Date</h6>
+        <DatePicker selected={resultDate} dateFormat="dd MMM yyyy" />
+      </div>
     </div>
   );
 }
